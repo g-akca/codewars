7467a206185f8633dd0b551bf66ea2e1a0a36196
g-akca <gok.akca66@gmail.com>
2025-08-30 22:23:35 +0300

    8kyu - Sum of positive (C#)

---
+++ 8kyu/Sum of positive/Solution.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Kata
+{
+  public static int PositiveSum(int[] arr)
+  {
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++) {
+      if (arr[i] > 0) sum += arr[i];
+    }
+    return sum;
+  }
+}
