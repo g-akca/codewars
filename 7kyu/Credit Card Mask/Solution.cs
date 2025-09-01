73e879a1cbe0105dda4e0951d2f3fe2b65c0c2de
g-akca <gok.akca66@gmail.com>
2025-09-01 14:15:15 +0300

    7kyu - Credit Card Mask (C#)

---
+++ 7kyu/Credit Card Mask/Solution.cs
@@ -0,0 +1,13 @@
+public static class Kata
+{
+  // return masked string
+  public static string Maskify(string cc)
+  {
+    string masked = "";
+    for (int i = 0; i < cc.Length; i++) {
+      if (i < cc.Length - 4) masked += '#';
+      else masked += cc[i];
+    }
+    return masked;
+  }
+}
