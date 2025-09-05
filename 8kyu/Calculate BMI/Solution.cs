20a07c94b70a60029db44b15f0cca9e978e419fb
g-akca <gok.akca66@gmail.com>
2025-09-05 18:30:03 +0300

    8kyu - Calculate BMI (C#)

---
+++ 8kyu/Calculate BMI/Solution.cs
@@ -0,0 +1,12 @@
+public class Kata
+{
+  public static string Bmi(double weight, double height)
+  {
+    double bmi = weight / (height * height);
+    
+    if (bmi <= 18.5) return "Underweight";
+    else if (bmi <= 25) return "Normal";
+    else if (bmi <= 30) return "Overweight";
+    return "Obese";
+  }
+}
