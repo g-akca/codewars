8f5524158b2e60b0d6791799ed97c90b84bfa941
g-akca <gok.akca66@gmail.com>
2025-09-06 10:01:12 +0300

    8kyu - Twice as old (C#)

---
+++ 8kyu/Twice as old/Solution.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Solution
+{
+  public class TwiceAsOldSolution
+  {
+    public static int TwiceAsOld(int dadYears, int sonYears) => Math.Abs(dadYears - 2 * sonYears);
+  }
+}
